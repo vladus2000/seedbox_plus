c8eef85595a46223d74ec5924e9201e4acca708d
vladus2000 <docker@matt.land>
2020-08-02 20:19:58 -0500

    fixing startup

--- shiz/startup_plus.sh
+++ shiz/startup_plus.sh
@@ -4,6 +4,6 @@ systemd-machine-id-setup
 touch /dbus.env
 chmod 666 /dbus.env
 /runevilorroot.sh 'dbus-launch > /dbus.env'
-/runevilorroot.sh '. /dbus.env && vncserver :0'
-/loop.sh /runevilorroot.sh '. /dbus.env && /usr/bin/pulseaudio'
+/runevilorroot.sh '. /dbus.env && vncserver :0' &
+/loop.sh /runevilorroot.sh '. /dbus.env && /usr/bin/pulseaudio' &
 /startup.sh
