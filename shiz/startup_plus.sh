9cde5269245fec979693aca028ce7d1342195455
vladus2000 <docker@matt.land>
2020-08-02 21:21:12 -0500

    more startup tweaks

--- shiz/startup_plus.sh
+++ shiz/startup_plus.sh
@@ -1,9 +1,11 @@
 systemd-machine-id-setup
 /usr/sbin/sshd
-#dbus-daemon --system
-touch /dbus.env
+dbus-daemon --system
+touch /dbusr.env
+chmod 666 /dbusr.env
+/runevilorroot.sh 'dbus-launch > /dbus.envr'
+sed -e 's/^/export /' /dbus.envr > /dbus.env 
 chmod 666 /dbus.env
-/runevilorroot.sh 'dbus-launch > /dbus.env'
 /runevilorroot.sh '. /dbus.env && vncserver :0' &
 /loop.sh /runevilorroot.sh '. /dbus.env && /usr/bin/pulseaudio' &
 /startup.sh
